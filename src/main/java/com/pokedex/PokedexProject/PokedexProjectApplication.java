b21995df63c8b4765a635d50003046e8db2bc8a4
0285douglas <0285douglas@gmail.com>
2025-04-27 13:05:12 -0300

    criação do projeto spring

---
+++ src/main/java/com/pokedex/PokedexProject/PokedexProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pokedex.PokedexProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexProjectApplication.class, args);
+	}
+
+}
